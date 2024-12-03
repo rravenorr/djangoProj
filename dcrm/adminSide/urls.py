2ea67278874fd5ed14f998889207c71cb1af5796
rravenorr <148618881+rravenorr@users.noreply.github.com>
2024-12-03 16:23:12 +0800

    1

--- dcrm/adminSide/urls.py
+++ dcrm/adminSide/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.login_user, name='login'),
+    path('home/', views.home, name='home'),
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
     path('shiftchange/', views.shiftC, name='shiftchange'),
     path('notifications/', views.notifications, name='notifications'),
 ]
--- dcrm/adminSide/views.py
+++ dcrm/adminSide/views.py
@@ -1,23 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
+
+
+
 def home(request):
+    return render(request, 'home.html', {})
+
+def login_user(request):
     #log in check
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             messages.success(request, "You have successfully logged in.")
-            return render(request, 'home.html', {})
+            return redirect('home')
         else:
             messages.success(request, "There was an error logging in. Please try again.")
-            return render(request, 'home.html', {})
-    else:
-        return render(request, 'home.html', {})
+            return render(request, 'login.html', {})
+    
+    return render(request, 'login.html', {})
     
 def shiftC(request):
     return render(request, 'shift.html', {})
@@ -28,4 +36,17 @@ def notifications(request):
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out.")
-    return redirect('home')
+    return redirect('login')
+
+def register_user(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
